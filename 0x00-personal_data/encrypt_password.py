7b58389783c0c2684197c50fe9876430795cd15b
abdallhfattah <abdallh.fattah33@gmail.com>
2024-09-13 20:02:03 +0300

    encrypted password

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""module to encrypt password before storing it inside of the database"""
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    # converting password to array of bytes
+    bytes = password.encode("utf-8")
+
+    # generating the salt
+    salt = bcrypt.gensalt()
+
+    return bcrypt.hashpw(bytes, salt)
